2b5689441b0a1f8c0e17e8dc97d5c9226673935e
Josh-Wickens <joshwickens@hotmail.com>
2022-10-08 19:09:26 +0000

    add first model for tables

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Tables(models.Model):
+    table_number = models.IntegerField(unique=True)
+    seats = models.IntegerField(max_length=6)
+    window = models.BooleanField()
