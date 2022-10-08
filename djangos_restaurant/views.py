42b482689508de8acc784027691b63ca18fdd1e4
Josh-Wickens <joshwickens@hotmail.com>
2022-10-08 18:49:08 +0000

    add html pages to files with simple html code and set up URLS

--- djangos_restaurant/views.py
+++ djangos_restaurant/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Home')
+    # return HttpResponse('Home')
+    return render(request, 'index.html')
 
 def menu(request):
-    return HttpResponse('Menu')
+    #return HttpResponse('Menu')
+    return render(request, 'menu.html')
 
 def book_table(request):
-    return HttpResponse('Book a table')
+    #return HttpResponse('Book a table')
+    return render(request, 'book_table.html')
 
 def my_bookings(request):
-    return HttpResponse('My Bookings')
+    #return HttpResponse('My Bookings')
+    return render(request, 'my_bookings.html')
 
-    
