4b377b1fde7fbf7d4140c0425e28f4fbcbdfe8fb
Josh-Wickens <joshwickens@hotmail.com>
2022-10-12 21:02:37 +0000

    add styling to carousel and add simple footer html and css

--- booking_system/models.py
+++ booking_system/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Tables(models.Model):
-    table_number = models.IntegerField(unique=True)
-    seats = models.IntegerField(max_length=6)
-    window = models.BooleanField()
+# class Tables(models.Model):
+#     table_number = models.IntegerField(unique=True)
+#     seats = models.IntegerField(max_length=6)
+#     window = models.BooleanField()
