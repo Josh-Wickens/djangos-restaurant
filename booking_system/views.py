e33ebbd636caa1a813f48ee8484c422b421d2647
Josh-Wickens <joshwickens@hotmail.com>
2022-10-26 20:41:05 +0000

    amend booking view so that it checks availbility for time and date before submitting

--- booking_system/views.py
+++ booking_system/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .models import FoodMenu, Reservation
 from .forms import ReserveTableForm
 from datetime import timedelta
+from django.contrib import messages
 
 # Create your views here.
 
@@ -34,14 +35,17 @@ def FoodMenu(request):
     
 
 
-def check_availablity(requested_date, requested_time):
-    no_tables_booked = len(Reservation.objects.filter(
-        check_in==requested_time,
-        date=requested_date, status="confirmed"))
-
-    # Return number of tables
-    # return no_tables_booked
-    print(check_availablity())
+# def check_availablity(requested_date, requested_time):
+#     no_tables_booked = len(Reservation.objects.filter(
+#         check_in=requested_time,
+#         date=requested_date, status="confirmed"))
+    
+#     return no_tables_booked
+    
+    # if no_tables_booked >= 10:
+    #     print("too many tables")
+    # else:
+    #     print(no_tables_booked)
 
 
 
@@ -53,9 +57,17 @@ def book_table(request):
 
         if reserve_form.is_valid():
             booking = reserve_form.save(commit=False)
-            booking.user = request.user
-            booking.save()
-            return render(request, 'my_bookings.html')      
+            # check_availablity(booking.date, booking.check_in)
+            no_tables_booked = len(Reservation.objects.filter(
+                check_in=booking.check_in,
+                date=booking.date, status="confirmed"))
+
+            if no_tables_booked >= 10:
+                print(no_tables_booked)
+            else:
+                booking.user = request.user
+                booking.save()
+                return render(request, 'my_bookings.html')      
             
     context = {'form': reserve_form}
 
