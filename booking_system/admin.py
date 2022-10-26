9cab3ba7348115cc6affbf5b8cbd72f491dbee48
Josh-Wickens <joshwickens@hotmail.com>
2022-10-26 17:55:26 +0000

    add filtering and search features to admin display

--- booking_system/admin.py
+++ booking_system/admin.py
@@ -4,6 +4,12 @@ from .models import FoodMenu, Table, Reservation
 
 # Register your models here.
 
+class Filter(admin.ModelAdmin):
+    list_display = ("date", "check_in", "status", "table", "reservation_id")
+    list_filter = ("date", "check_in", "status", "table", "reservation_id")
+    search_fields = ("date", "check_in", "reservation_id", "user",)
+
+
 admin.site.register(FoodMenu)
 admin.site.register(Table)
-admin.site.register(Reservation)
+admin.site.register(Reservation, Filter)
